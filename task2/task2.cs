447b2b2e0f99df55225a42851103be705bd166f2
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-09-11 19:01:07 +0300

    task2

---
+++ task2/task2.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает позиции (две) элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.Clear();
+
+Random rnd = new Random();
+int lines = rnd.Next(3, 9);
+int col = rnd.Next(3, 9);
+int[,] arr = new int[lines, col];
+
+for (int i = 0; i < lines; i++)
+{
+    for (int j = 0; j < col; j++)
+    {
+        arr[i, j] = rnd.Next(10);
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите строку:   ");
+int linePos = Int32.Parse(Console.ReadLine()) - 1;
+Console.Write("Введите столбец:  ");
+int colPos = Int32.Parse(Console.ReadLine()) - 1;
+
+Console.WriteLine((linePos <= lines && colPos <= col)
+                    ? arr[linePos, colPos]
+                    : "Нет такой позиции в массиве");
+
