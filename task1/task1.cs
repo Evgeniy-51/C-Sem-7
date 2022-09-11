1e3d1d733ee7957b49900793cfb4cd9a5ae8d434
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-09-11 18:13:20 +0300

    task1

---
+++ task1/task1.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными (тип double) числами.
+
+Console.Clear();
+
+Console.Write("Введите число M:  ");
+int m = Int32.Parse(Console.ReadLine());
+Console.Write("Введите число N:  ");
+int n = Int32.Parse(Console.ReadLine());
+
+Random rnd = new Random();
+double[,] arr = new double[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i, j] = Math.Round(rnd.NextDouble() * 10, 2);
+        System.Console.Write(arr[i, j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+
