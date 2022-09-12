c11a33c93f2017eabc69a2ba7c21effcb03964f2
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-09-13 01:18:46 +0300

    task3

---
+++ task3/task3.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+
+Random rnd = new Random();
+int lines = rnd.Next(3, 6);
+int columns = rnd.Next(3, 6);
+double[,] arr = new double[lines, columns];
+double[] avgs = new double[columns];
+
+for (int i = 0; i < lines; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        arr[i, j] = rnd.Next(1, 10);
+        avgs[j] += Convert.ToDouble(arr[i, j]) / lines;
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("_______________________________________________");
+
+foreach (double elem in avgs)
+{
+    Console.Write(Math.Round(elem, 2) + "\t");
+}
